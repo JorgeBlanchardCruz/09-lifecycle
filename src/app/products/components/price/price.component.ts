62ce7782d61761745e75683799f41f59519d21c1
Jorge Blanchard Cruz <rothgard@live.com>
2024-05-19 19:22:24 +0100

    next

--- src/app/products/components/price/price.component.ts
+++ src/app/products/components/price/price.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'products-price',
   templateUrl: './price.component.html',
   styleUrl: './price.component.css'
 })
-export class PriceComponent {
+export class PriceComponent implements OnInit, OnChanges, OnDestroy {
+
+  @Input()
+  public price: number = 0;
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    console.log('PRICECOMPONENT: ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log({ changes });
+    console.log('PRICECOMPONENT: ngOnChanges');
+  }
+
+  ngOnDestroy(): void {
+    console.log('PRICECOMPONENT: ngOnDestroy');
+  }
 
 }
